67f8f5542c964b86227539e1ba4859960a1c2012
mujahidahkamilah <19821105@mahasiswa.itb.ac.id>
2022-06-09 18:15:03 +0700

    add kendaraan.hpp & kendaraan.cpp

---
+++ Kendaraan.hpp
@@ -0,0 +1,30 @@
+#ifndef __KENDARAAN__
+#define __KENDARAAN__
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Kendaraan {
+    private:
+        int nomor;
+        int tahun_keluaran;
+        string merk;
+        string kategori;
+        int lamaSewa;
+        static int sewa_perjam;
+
+    public:
+        Kendaraan();
+        Kendaraan(int nomor, int tahun_keluaran, string merk, string kategori);
+        Kendaraan(const Kendaraan& kendaraan);
+        Kendaraan& operator=(const Kendaraan &k);
+        ~Kendaraan();
+
+        void printInfo();
+
+        void biayaSewa(int lamaSewa);
+
+};
+
+#endif
