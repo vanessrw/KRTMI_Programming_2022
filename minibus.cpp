bd11959056e33e6613dd823fbc4cab62840725c1
hannyaritra <73069670+hannyaritra@users.noreply.github.com>
2022-06-09 13:44:53 +0700

    Create minibus.cpp
    masih banyak kekurangan

---
+++ minibus.cpp
@@ -0,0 +1,33 @@
+/******* Hanny Margaretha Aritonang *******/
+
+#include "minibus.hpp"
+
+int Minibus:: hargaSewa = 1000000;
+
+Minibus :: Minibus() {
+    this-> nomor = 0;
+    this-> tahun_keluaran = 0;
+    this-> merk = "";
+    this-> kategori = "mobil";
+    this-> lamaSewa = new int[hargaSewa];
+    //this-> biayaSewa = 0;
+}
+
+Minibus :: Minibus (int nomor, int tahun_keluaran, string merk, string kategori):
+nomor(nomor), tahun_keluaran(tahun_keluaran), merk(merk), kategori(kategori){
+    this -> lamaSewa = new int[hargaSewa];
+    //this-> biayaSewa = 0;
+}
+
+void Minibus :: printInfo(){
+    cout << "Nomor : " << this-> nomor << endl;
+    cout << "Tahun Keluaran : " << this-> tahun_keluaran << endl;
+    cout << "Merk : " << this-> merk << endl;
+    cout << "Kategori : " << this-> kategori << endl;
+    cout << endl;
+}
+
+//void Minibus:: biayaSewa(int lamaSewa){
+    //biayaSewa = lamaSewa * hargaSewa;
+//}
+
