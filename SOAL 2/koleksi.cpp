f852b6644b73d77c73bb75a013f725dbbce6cf06
vanessrw <64979714+vanessrw@users.noreply.github.com>
2022-06-10 10:29:46 +0700

    soal 2

---
+++ SOAL 2/koleksi.cpp
@@ -0,0 +1,58 @@
+#include "koleksi.h"
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+int Kendaraan::n_kendaraan = 0;
+
+Kendaraan::Kendaraan(char *n){
+    this->name = new char[strlen(n)];
+    strcpy(this->name, n);
+    this->num_of_kendaraan = 0;
+    this->kendaraan_list = new char *[5000];
+    n_kendaraan++;
+}
+
+Kendaraan::Kendaraan(const Kendaraan &Kendaraan){
+    this->name = new char[strlen(Kendaraan.getName())];
+    strcpy(this->name, kendaraan.getName());
+    this->num_of_kendaraan = kendaraan.num_of_kendaraan;
+    this->kendaraan_list = new char *[255];
+    for (int i = 0; i < Kendaraan.num_of_kendaraan; i++)
+    {
+        this->kendaraan_list[i] = new char[strlen(Kendaraan.kendaraan_list[i])];
+        strcpy(this->kendaraan_list[i], Kendaraan.kendaraan_list[i]);
+    }
+    n_kendaraan++;
+}
+
+// menambah kendaraan sesuai kapasitas array
+void Kendaraan::addKendaraan(char *Kendaraan){
+    if (n_kendaraan < 101) {
+        this->num_of_kendaraan++;
+        this->kendaraan_list[this->num_of_kendaraan - 1] = new char[strlen(Kendaraan)];
+        strcpy(this->kendaraan_list[this->num_of_kendaraan - 1], Kendaraan);
+        }
+    else{
+        cout << "Gagal menambahkan kendaraan, list penuh !" << endl;
+    }
+}
+
+int Kendaraan::getNumOfKendaraan() const{
+    return this->num_of_kendaraan;
+}
+
+void Kendaraan::viewKendaraanList() const{
+    if (this->num_of_kendaraan > 0){
+        for (int i = 0; i < this->num_of_kendaraan; i++){
+            cout << (i + 1) << ". " << this->kendaraan_list[i] << endl;
+        }
+    }
+    else{
+        cout << "List kendaraan kosong." << endl;
+    }
+}
+
+int Kendaraan::getNumOfKendaraan(){
+    return Kendaraan::n_kendaraan;
+}
