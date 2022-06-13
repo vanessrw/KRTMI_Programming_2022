e4b2fec1b98982fad78571988a85e33e63710021
hannyaritra <73069670+hannyaritra@users.noreply.github.com>
2022-06-13 22:22:30 +0700

    Create kendaraan.hpp

---
+++ kendaraan.hpp
@@ -0,0 +1,30 @@
+/******** Hanny Margaretha Aritonang *********/
+
+#ifndef __KENDARAAN__
+#define __KENDARAAN__
+
+#include <iostream>
+using namespace std;
+
+class Kendaraan {
+    private :
+        int nomor;
+        int tahun_keluaran;
+        string merk;
+        string kategori;
+        
+    public :
+        Kendaraan();                                          //default-ctor
+        Kendaraan(int nomor, int tahun_keluaran, string merk, string kategori);   //user-defined-ctor
+        Kendaraan(const Kendaraan &k);
+        Kendaraan& operator=(const Kendaraan &k);
+        ~Kendaraan();
+
+    void printInfo();
+
+    long biayaSewa(int lamaSewa);
+
+};
+
+
+#endif
