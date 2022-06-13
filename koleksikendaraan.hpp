c482268ca596021f530c326c0f2f20686485aa13
hannyaritra <73069670+hannyaritra@users.noreply.github.com>
2022-06-13 21:52:26 +0700

    revisi koleksikendaraan.hpp

--- koleksikendaraan.hpp
+++ koleksikendaraan.hpp
@@ -3,40 +3,23 @@
 #ifndef __KOLEKSIKENDARAAN__
 #define __KOLEKSIKENDARAAN__
 
-#include <iostream>
+#include "kendaraan.hpp"
 using namespace std;
 
 class KoleksiKendaraan {
     private :
-        string arrKategori[3], arrMerk[3];
-        int arrTahun[100], arrNomor[100];
-        // yang ada hanya array of kendaraan, bukan array of string
-        // liat contoh yang benar pada branch mujah
+        Kendaraan* koleksi;
+        int size;
         int Neff;
-        // aloksasi memori dari array itu tidak dilakukan di sini
-        // tapi dilakukan di constructor, jadi cukup tuliskan seperti ini
-        // untuk membuat array of sesuatu
-        // sesuatu* arrName;
-        // nanti di konstrukor tinggal dialokasikan dengan atribut size seperti ini
-        // arrName = new sesuatu[size];
-        // sayangnya kamu tidak membuat atribut size
-
+    
     public :
         KoleksiKendaraan ();                                //default-ctor
-        KoleksiKendaraan (char array[100], int Neff);     //user-defined-ctor
-        // ee aku tidak mengerti kenapa kamu memasukkan array sebagai parameter
+        KoleksiKendaraan (int size);     //user-defined-ctor
         KoleksiKendaraan (const KoleksiKendaraan &kk);      //copy ctor
         ~KoleksiKendaraan();
-
-        void menu(); // menu gak perlu, tidak ada permintaan untuk membuat menu di sini
 
         void printAll();
 
 };
-
-struct Stack{
-
-}; // ini buat apa? lupa dihapus ea
-
 
 #endif
