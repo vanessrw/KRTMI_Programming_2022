31da21710d2a55d3ea6f4857c64b1a7f95228907
vanessrw <64979714+vanessrw@users.noreply.github.com>
2022-06-17 13:00:17 +0700

    update

--- SOAL 1/kendaraan.hpp
+++ SOAL 1/kendaraan.hpp
@@ -13,13 +13,9 @@ class Kendaraan {
 
         int*lama_sewa;
         static int harga_sewa;
-        // dua variabel tersebut sama seperti milik hanny
-        // komentar dari aku sama seperti pada hanny
     
     public :
-        Kendaraan()    // default-constructor 
-        // gak ada titik komanya di belakang, error nih kalau dicompile
-        
+        Kendaraan();    // default-constructor 
         Kendaraan(int nomor, int tahun_keluaran, string merk, string kategori);  //user-defined constructor
         Kendaraan(const Kendaraan &k);   // copy constructor
         Kendaraan& operator=(const Kendaraan &k);    // assignment operator
@@ -31,30 +27,5 @@ class Kendaraan {
         static int get_harga_sewa();
         long biayaSewa(int lamaSewa);
 };
-
-// PER JENIS KENDARAAN
-struct bus{
-    int bus_id;
-    int bus_year;
-    string bus_merk;
-    string bus_kategori;
-};
-struct minibus{
-    int minibus_id;
-    int minibus_year;
-    string minibus_merk;
-    string minibus_kategori;
-};
-
-struct car{
-    int car_id;
-    int car_year;
-    string car_merk;
-    string car_kategori;
-};
-
-// tidak ada perintah untuk membuat ADT untuk setiap kategori kendaraan
-// kategori kendaraan cukup berupa string saja
-// kenapa kamu mempersulit diri sendiri :v
 
 #endif
